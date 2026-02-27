e652dac7515ee44eb8af05d1ef17f70a4524cf80
JL <joseluis.mendoza4@murciaeduca.es>
2026-02-27 17:44:58 +0100

    cambio base de datos

--- modelo/Basedatos.php
+++ modelo/Basedatos.php
@@ -2,29 +2,22 @@
 
 abstract class Basedatos
 {
-    private $servername = "10.227.192.20:3306";
-    private $database = "sanitariabd";
-    private $username = "sanitariabd";
-    private $password ="sanitariabd";
+    private $servername = "192.168.35.124:3306";
+    private $database = "sanidadPHP";
+    private $username = "sanidadphp";
+    private $password = "1234";
     private $conexion;
     private $mensajeerror = "";
-
-
-    // private $servername = "localhost";
-    // private $database = "sanitaria";
-    // private $username = "root";
-    // private $password = "";
-    // private $conexion;
-    // private $mensajeerror = "";
-
-    # Conectar a la base de datos
-
     public function getConexion()
     {
-
         try {
+            // Extraer host y puerto si vienen juntos (ej: 192.168.1.1:3306)
+            $parts = explode(':', $this->servername);
+            $host = $parts[0];
+            $port = isset($parts[1]) ? ";port=" . $parts[1] : "";
+
             $this->conexion = new PDO(
-                "mysql:host=$this->servername;dbname=$this->database;charset=utf8",
+                "mysql:host=$host$port;dbname=$this->database;charset=utf8",
                 $this->username,
                 $this->password
             );
@@ -32,6 +25,8 @@ abstract class Basedatos
             return $this->conexion;
         } catch (PDOException $e) {
             $this->mensajeerror = $e->getMessage();
+            error_log("Fallo de conexión: " . $this->mensajeerror);
+            return null;
         }
     }
 
